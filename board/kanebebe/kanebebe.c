dd82d343a89ad3819b612e92e0e1c843abd6a5a8
Yoshinori Sato <ysato@users.sourceforge.jp>
2010-09-27 11:53:14 +0900

    Add kanebebe.

---
+++ board/kanebebe/kanebebe.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010 Yoshinori Sato <ysato@users.sourceforge.jp>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <net.h>
+#include <netdev.h>
+#include <asm/io.h>
+#include <asm/processor.h>
+
+int checkboard(void)
+{
+	puts("BOARD: csun KaneBebe\n");
+	return 0;
+}
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	DECLARE_GLOBAL_DATA_PTR;
+
+	gd->bd->bi_memstart = CONFIG_SYS_DRAM_BASE;
+	gd->bd->bi_memsize = CONFIG_SYS_DRAM_SIZE;
+	printf("DRAM:  %dMB\n", CONFIG_SYS_DRAM_SIZE / (1024 * 1024));
+	return 0;
+}
+
+void led_set_state(unsigned short value)
+{
+	unsigned char padr;
+	padr = *(unsigned char *)0xffffd9;
+	padr &= 0x3f;
+	padr |= (value & 0x03) << 6;
+	*(unsigned char *)0xffffd9 = padr;
+}
+
+#ifdef CONFIG_CMD_NET
+int board_eth_init(bd_t *bis)
+{
+	return rtl8019_initialize(bis, CONFIG_RTL8019_BASE);
+}
+#endif
---
+++ include/configs/kanebebe.h
@@ -0,0 +1,100 @@
+/*
+ * Configuation settings for the csun Kane Bebe
+ *
+ * Copyright (C) 2010 Yoshinori Sato
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __KANEBEBE_H
+#define __KANEBEBE_H
+
+#undef DEBUG
+#define CONFIG_H8300		1
+#define CONFIG_H8300H		1
+#define CONFIG_CPU_H83069	1
+#define CONFIG_KANEBEBE	1
+
+#include <config_cmd_default.h>
+#undef CONFIG_CMD_BDI
+#undef CONFIG_CMD_FPGA
+#undef CONFIG_CMD_IMI
+#undef CONFIG_CMD_IMLS
+#undef CONFIG_CMD_NFS
+#undef CONFIG_CMD_SDRAM
+#undef CONFIG_CMD_CACHE
+
+#define CONFIG_NET_MULTI
+
+#define CONFIG_BAUDRATE		38400
+#define CONFIG_BOOTARGS		"console=ttySC0,115200"
+
+#define CONFIG_VERSION_VARIABLE
+#undef	CONFIG_SHOW_BOOT_PROGRESS
+
+/* MEMORY */
+#define KANEBEBE_DRAM_BASE	0x400000
+
+#define CONFIG_SYS_LONGHELP		/* undef to save memory	*/
+#define CONFIG_SYS_PROMPT	"=> "	/* Monitor Command Prompt */
+#define CONFIG_SYS_CBSIZE	256	/* Buffer size for input from the Console */
+#define CONFIG_SYS_PBSIZE	256	/* Buffer size for Console output */
+#define CONFIG_SYS_MAXARGS	16	/* max args accepted for monitor commands */
+/* Buffer size for Boot Arguments passed to kernel */
+#define CONFIG_SYS_BARGSIZE	512
+/* List of legal baudrate settings for this board */
+#define CONFIG_SYS_BAUDRATE_TABLE	{ 38400 }
+
+/* SCI */
+#define CONFIG_SCI_SERIAL	1
+#define CONFIG_SCI_BASE		0xffffb8	/* SCI1 */	
+
+/* TIMER */
+#define CONFIG_TIMER_BASE	0xffff80	/* CH0-1 */
+
+#define CONFIG_WATCHDOG
+
+#define CONFIG_SYS_MEMTEST_START	KANEBEBE_DRAM_BASE
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + (4 * 1024 * 1024))
+
+#define CONFIG_SYS_DRAM_BASE	        KANEBEBE_DRAM_BASE
+#define CONFIG_SYS_DRAM_SIZE		(4 * 1024 * 1024)
+
+#define CONFIG_SYS_LOAD_ADDR		0x500000
+
+#define CONFIG_SYS_MALLOC_LEN		(256 * 1024)
+#define CONFIG_SYS_GBL_DATA_SIZE	256
+
+#define CONFIG_ENV_SIZE			1024
+#define CONFIG_ENV_IS_IN_FLASH
+#define CONFIG_ENV_ADDR			0x001000
+
+#define CONFIG_SYS_RX_ETH_BUFFER	1
+
+#define CONFIG_SYS_MAX_FLASH_BANKS	1
+#define CONFIG_SYS_MAX_FLASH_SECT	7
+#define CONFIG_SYS_FLASH_BASE		0x001000
+
+/* Board Clock */
+#define CONFIG_SYS_HZ			25000000
+
+#define CONFIG_DRIVER_RTL8019
+#define CONFIG_RTL8019_BASE		0x200000
+
+#endif
