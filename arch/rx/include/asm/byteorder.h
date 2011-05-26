ab74752b3f933ddc0272e8cb29413cb549ee4d06
Yoshinori Sato <ysato@users.sourceforge.jp>
2011-05-26 23:50:59 +0900

    add 64bit define

--- arch/rx/include/asm/byteorder.h
+++ arch/rx/include/asm/byteorder.h
@@ -1,6 +1,9 @@
 #ifndef _RX_BYTEORDER_H
 #define _RX_BYTEORDER_H
 
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
+#define __BYTEORDER_HAS_U64__
+#endif
 #include <linux/byteorder/little_endian.h>
 
 #endif /* _RX_BYTEORDER_H */
