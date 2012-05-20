ff88bdd1d713a5f9929d3e3a56e5e4b06795ddfe
Yoshinori Sato <ysato@users.sourceforge.jp>
2012-05-21 01:41:33 +0900

    fix endian

--- arch/rx/include/asm/unaligned.h
+++ arch/rx/include/asm/unaligned.h
@@ -4,7 +4,7 @@
 #include <linux/unaligned/access_ok.h>
 #include <linux/unaligned/generic.h>
 
-#define get_unaligned	__get_unaligned_be
-#define put_unaligned	__put_unaligned_be
+#define get_unaligned	__get_unaligned_le
+#define put_unaligned	__put_unaligned_le
 
 #endif /* _ASM_RX_UNALIGNED_H */
